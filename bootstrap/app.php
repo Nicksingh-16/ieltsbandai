5cb74618c211fc809099a658cfb9b1ce08ccdd9e
Nicksingh-16 <nishant.shekhawat@webitoinfotech.com>
2026-04-15 01:15:02 +0530

    fix: trust all proxies so Laravel detects HTTPS correctly behind Render load balancer

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'check.credits' => \App\Http\Middleware\CheckTestCredits::class,
             'admin'         => \App\Http\Middleware\AdminMiddleware::class,
